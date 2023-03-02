5b4a1fa5cf405f40b1235fd21324ecfe3cbb3bb5
Juan Hidalgo <juan.hidalgo@uao.edu.co>
2023-03-02 13:00:34 -0500

    Challenge3 agregar categorias

--- firstProjectWeb/src/main.jsx
+++ firstProjectWeb/src/main.jsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import MyFirstApp from './MyFirstApp'
 import Counter from './Counter'
+import GifExpertApp from './ComponentApp'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Counter value = {100} />
+   <GifExpertApp/>
   </React.StrictMode>,
 )
